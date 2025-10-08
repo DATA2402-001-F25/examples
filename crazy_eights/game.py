4cf84d259dc9e890dd851cd84d306e0e77f55cd7
sreeragvadde <sreeragvadde@gmail.com>
2025-10-08 16:57:44 -0600

    game

--- crazy_eights/game.py
+++ crazy_eights/game.py
@@ -1,18 +1,23 @@
-
-
+from crazy_eights import player
+l=[['a',10],['b',12],[',c',15]]
+for i in l:
+    pass
 class CrazyEightsGame:
 
     def __init__(self):
-        pass # your code here
-
+        self.player_List=[]
+        
     def add_player(self):
         """
         Ask a player to enter (at the keyboard) their name
         Create a Player object for them, and add it to the list of players for this game
         """
-        pass # your code here
+        name=str(input("enter your name"))
+        player=Player(name,0)
+        self.playerList.append(player)
+        # your code here
 
-    def play_round(self):
+    def play_round(self,deck):
         """
         play one round of crazy eights
         Reset/shuffle the deck and deal each player their starting hand.
@@ -20,4 +25,22 @@ class CrazyEightsGame:
         (make use of Hand.search and Player.choose_card)
         When a player wins the round, update their score
         """
+        deck.reset()
+        deck.shuffle()
+        for player in self.player_List:
+            for i in range(5):
+                player.hand.add(deck.deal())
+            top_card=deck.deal()
+        playing=True
+        while playing:
+            for i in self.player_List[0:1]:
+                potential_cards=player.hand.search()
+                while len(potential_cards) == 0:
+                    try:
+                        player.deck.deal()
+                        potential_cards=player.hand.search()
+                    except:
+                        print("no cards left")
+                player.choose_card(potential_cards)
+    
         pass # your code here
