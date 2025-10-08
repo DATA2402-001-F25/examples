213e2b30a16020849278d1659fa5306fefa148d1
Misaa141 <misaa141@mtroyal.ca>
2025-10-08 16:59:15 -0600

    player class finished

--- crazy_eights/player.py
+++ crazy_eights/player.py
@@ -5,7 +5,9 @@ from hand import Hand
 class Player:
 
     def __init__(self, name: str):
-        pass # your code here
+        self.name = name 
+        self.hand = Hand()
+        self.points = 0
 
     def choose_card(self, starter: Card) -> Card | None:
         """
@@ -13,5 +15,19 @@ class Player:
         present them with a list of legal options (make use of Hand.search), but also give them the option to play nothing (draw)
         return the selected Card object, or return None if no legal plays are possible or the player chooses to draw.
         """
-        pass # your code here
+        
+        legal_plays = self.hand.search(starter)
+        print(legal_plays)
+
+        play = input(f'Choose a card to play, or write "None" to skip.')
+
+        if play == "None":
+            return None
+        
+        return legal_plays[int(play)]
+
+            
+
+       
+        
 
